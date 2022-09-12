32d0ea41736e066049f5143d8d67cdf4920c71e4
Ifka Nabilah <85624063+zoroaster-dot@users.noreply.github.com>
2022-09-12 20:58:58 +0700

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,1 +1,1 @@
-export const API = "https://api.currencyfreaks.com/latest?apikey=9a69648e96e5434786d1d49b012d13d2&symbols=PKR,GBP,EUR,USD";
+export const API = "https://api.currencyfreaks.com/latest?apikey=9a69648e96e5434786d1d49b012d13d2&symbols=CAD, EUR, IDR, JPY, CHF, GBP";
